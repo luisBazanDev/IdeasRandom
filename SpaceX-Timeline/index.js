ab84d4071eac2854c294a357cebe37278c15d52f
Luis Bazan <luis@bazan.pe>
2023-06-04 19:21:19 -0500

    Implement events in timeline

--- SpaceX-Timeline/index.js
+++ SpaceX-Timeline/index.js
@@ -1,14 +1,45 @@
 const element = document.getElementById("timeline");
 
-let time_seconds = -12;
+let time_seconds = -5;
 let stage = "STARLINK";
+
+const events = [
+  {
+    time: 0,
+    name: "STARTUP",
+    up: false,
+  },
+  {
+    time: 30,
+    name: "LITOFF",
+    up: true,
+  },
+  {
+    time: 80,
+    name: "MAX-Q",
+    up: false,
+  },
+  {
+    time: 150,
+    name: "MECO",
+    up: true,
+  },
+  {
+    time: 175,
+    name: "FAIRING",
+    up: false,
+  },
+  {
+    time: 450,
+    name: "AWS",
+    up: true,
+  },
+];
 
 const drawCanvas = () => {
   const ctx = element.getContext("2d");
   const width = element.width;
   const height = element.height;
-
-  console.log(width, height);
 
   ctx.clearRect(0, 0, width, height);
 
@@ -33,7 +64,7 @@ const drawCanvas = () => {
   ctx.beginPath();
   ctx.strokeStyle = "#4f4f4f";
   ctx.lineWidth = 4;
-  ctx.arc(width / 2, width * 1.2, width, 0, parseHexRad(360), false);
+  ctx.arc(width / 2, width * 1.2, width, 0, parseHexRad(300), false);
   ctx.stroke();
 
   // Progress
@@ -44,18 +75,89 @@ const drawCanvas = () => {
   ctx.stroke();
 
   // Time
+  ctx.textAlign = "start";
   ctx.fillStyle = "#fff";
-  ctx.font = "44px Arial";
+  ctx.font = "40px Poppins";
   let text = `T${time_seconds < 0 ? "- " : "+ "}`;
   text += formatTime(Math.floor(Math.abs(time_seconds) / (60 * 60)) % 24) + ":"; // Hours
   text += formatTime(Math.floor(Math.abs(time_seconds) / 60) % 60) + ":"; // Minutes
   text += formatTime(Math.abs(time_seconds) % 60); // Secconds
-  ctx.fillText(text, width / 2 - (text.length / 2) * 22, 350);
+  ctx.fillText(text, width / 2 - (text.length / 2) * 20, 350);
 
   // Stage
   ctx.fillStyle = "#aaa";
-  ctx.font = "25px Arial";
+  ctx.font = "25px Poppins";
   ctx.fillText(stage, width / 2 - (stage.length / 2) * 15, 390);
+
+  printPoints({
+    ctx,
+    time: time_seconds,
+    width,
+    height,
+    radius: 2,
+  });
+};
+
+const printPoints = ({ ctx, time, height, width, radius }) => {
+  const lapse = 60 * 9;
+  for (const event of events) {
+    if (time - event.time > lapse / 2 || event.time - time > lapse / 2)
+      continue;
+    const relativeTime = event.time - time;
+    const x = map_range(lapse / 2 + relativeTime, 0, lapse, 0, width);
+    const y_angle = map_range(lapse / 2 + relativeTime, 0, lapse, 240, 300);
+    const y = width * 1.2 + Math.sin(parseHexRad(y_angle)) * width;
+
+    ctx.beginPath();
+    ctx.lineWidth = 2;
+    ctx.moveTo(x, y);
+    if (event.up) {
+      ctx.lineTo(
+        x + Math.cos(parseHexRad(y_angle)) * 13,
+        y + Math.sin(parseHexRad(y_angle)) * 13
+      );
+
+      ctx.textAlign = "center";
+      ctx.fillStyle = "#fff";
+      ctx.font = "10px Poppins";
+      ctx.fillText(
+        event.name,
+        x + Math.cos(parseHexRad(y_angle)) * 20,
+        y + Math.sin(parseHexRad(y_angle)) * 20
+      );
+    } else {
+      ctx.lineTo(
+        x - Math.cos(parseHexRad(y_angle)) * 13,
+        y - Math.sin(parseHexRad(y_angle)) * 13
+      );
+
+      ctx.textAlign = "center";
+      ctx.fillStyle = "#fff";
+      ctx.font = "12px Poppins";
+      ctx.fillText(
+        event.name,
+        x - Math.cos(parseHexRad(y_angle)) * 26,
+        y - Math.sin(parseHexRad(y_angle)) * 26
+      );
+    }
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.strokeStyle = "#fff";
+    ctx.fillStyle = "#000";
+    ctx.lineWidth = 2;
+    ctx.arc(x, y, 8, 0, 2 * Math.PI, false);
+    ctx.fill();
+    ctx.stroke();
+    if (lapse / 2 + relativeTime <= lapse / 2) {
+      ctx.beginPath();
+      ctx.fillStyle = "#fff";
+      ctx.arc(x, y, 3, 0, 2 * Math.PI, false);
+      ctx.fill();
+    }
+
+    // console.log(x, y, height, Math.sin(y_angle));
+  }
 };
 
 const parseHexRad = (hex) => {
@@ -67,12 +169,21 @@ const formatTime = (time) => {
 };
 
 setInterval(() => {
-  time_seconds++;
   drawCanvas();
+}, 1000 / 60);
+
+setInterval(() => {
+  time_seconds++;
+  for (const event of events) {
+    if (time_seconds >= event.time) stage = event.name;
+  }
 }, 1000);
 
 drawCanvas();
 
+function map_range(value, low1, high1, low2, high2) {
+  return low2 + ((high2 - low2) * (value - low1)) / (high1 - low1);
+}
 
 /*
  Esto fue solo una prueba que queria hacer,
